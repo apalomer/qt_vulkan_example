92a1324e618849bb2dd7a5768a1f7e3220894cc6
Albert Palomer <albert.palomer@iquarobotics.com>
2022-04-20 08:43:47 +0200

    First commit

---
+++ src/hellovulkanwindow.h
@@ -0,0 +1,27 @@
+#include <QVulkanWindow>
+
+class VulkanRenderer : public QVulkanWindowRenderer
+{
+public:
+    VulkanRenderer(QVulkanWindow *w);
+
+    void initResources() override;
+    void initSwapChainResources() override;
+    void releaseSwapChainResources() override;
+    void releaseResources() override;
+
+    void startNextFrame() override;
+
+private:
+    QVulkanWindow *m_window;
+    QVulkanDeviceFunctions *m_devFuncs;
+    QColor background_max_color = Qt::green;
+    int iteration = 0;
+    int max_iterations = 1000;
+};
+
+class VulkanWindow : public QVulkanWindow
+{
+public:
+    QVulkanWindowRenderer *createRenderer() override;
+};
